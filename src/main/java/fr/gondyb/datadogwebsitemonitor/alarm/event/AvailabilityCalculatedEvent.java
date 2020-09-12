cf0e8f2f193f6ca243bafd598a631d2ffec0bfd7
Benjamin Gondange <benjamin.gondange@reseau.eseo.fr>
2020-09-12 16:25:48 +0200

    Documented Events

--- src/main/java/fr/gondyb/datadogwebsitemonitor/alarm/event/AvailabilityCalculatedEvent.java
+++ src/main/java/fr/gondyb/datadogwebsitemonitor/alarm/event/AvailabilityCalculatedEvent.java
@@ -4,12 +4,27 @@ import lombok.Getter;
 
 import java.net.URI;
 
+/**
+ * This immutable class is an event informing that the availability for a website was calculated.
+ */
 @Getter
 public class AvailabilityCalculatedEvent {
+    /**
+     * The URI of the website concerned with the availability.
+     */
     private final URI uri;
 
+    /**
+     * The availability percentage calculated.
+     */
     private final double availability;
 
+    /**
+     * Class constructor
+     *
+     * @param uri          The URI of the website concerned with the availability
+     * @param availability The availability percentage calculated
+     */
     public AvailabilityCalculatedEvent(URI uri, double availability) {
         this.uri = uri;
         this.availability = availability;
